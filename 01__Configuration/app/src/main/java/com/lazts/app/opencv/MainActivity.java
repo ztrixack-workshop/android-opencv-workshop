8c81649c0dfb7d84c7db61eb7038d0dcbec175a9
tanawat <tanawat@sic.co.th>
2017-09-12 10:48:40 +0700

    add open cv config demo app

---
+++ 01__Configuration/app/src/main/java/com/lazts/app/opencv/MainActivity.java
@@ -0,0 +1,40 @@
+package com.lazts.app.opencv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    static {
+        if (OpenCVLoader.initDebug()) {
+            Log.d("OpenCVLoader", "SUCCESSFULLY LOADED");
+        } else {
+            Log.d("OpenCVLoader", "NOT LOADED");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
